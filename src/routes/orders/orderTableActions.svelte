afef0d55a0b70c8a5942c52563be4575d8f95c01
royid_poulose <royidcp96@gmail.com>
2024-03-13 16:53:44 +0530

    Order Table farame

---
+++ src/routes/orders/orderTableActions.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+    // import MoreHorizontal from "lucide-svelte/dist/icons/move-horizontal.svelte";
+    import { DotSquare } from "lucide-svelte";
+    import * as DropdownMenu from "$lib/components/ui/dropdown-menu";
+    import { Button } from "$lib/components/ui/button";
+   
+    export let id: string;
+  </script>
+   
+  <DropdownMenu.Root>
+    <DropdownMenu.Trigger asChild let:builder>
+      <Button
+        variant="ghost"
+        builders={[builder]}
+        size="icon"
+        class="relative h-8 w-8 p-0"
+      >
+        <span class="sr-only">Open menu</span>
+        <DotSquare class="h-4 w-4" />
+      </Button>
+    </DropdownMenu.Trigger>
+    <DropdownMenu.Content>
+      <DropdownMenu.Group>
+        <DropdownMenu.Label>Actions</DropdownMenu.Label>
+        <DropdownMenu.Item on:click={() => navigator.clipboard.writeText(id)}>
+          Copy payment ID
+        </DropdownMenu.Item>
+      </DropdownMenu.Group>
+      <DropdownMenu.Separator />
+      <DropdownMenu.Item>View customer</DropdownMenu.Item>
+      <DropdownMenu.Item>View payment details</DropdownMenu.Item>
+    </DropdownMenu.Content>
+  </DropdownMenu.Root>
