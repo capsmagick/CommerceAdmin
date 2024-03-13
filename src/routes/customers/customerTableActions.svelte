7e6f15d5f033c3682f8bcb613d2dbd53efcb2451
jishnu <jishnuks9421@gmail.com>
2024-03-13 18:14:16 +0530

    customer table added

---
+++ src/routes/customers/customerTableActions.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+    import { MoreHorizontal } from "lucide-svelte";
+    import * as DropdownMenu from "$lib/components/ui/dropdown-menu";
+    import { Button } from "$lib/components/ui/button";
+   
+    export let id: string;
+  </script>
+   
+  <DropdownMenu.Root>
+    <DropdownMenu.Trigger asChild let:builder>
+      <Button
+        variant="ghost"
+        builders={[builder]}
+        size="icon"
+        class="relative h-8 w-8 p-0"
+      >
+        <span class="sr-only">Open menu</span>
+        <MoreHorizontal class="h-4 w-4" />
+      </Button>
+    </DropdownMenu.Trigger>
+    <DropdownMenu.Content>
+      <DropdownMenu.Group>
+        <DropdownMenu.Label>Actions</DropdownMenu.Label>
+      </DropdownMenu.Group>
+      <DropdownMenu.Separator />
+      <DropdownMenu.Item>View customer</DropdownMenu.Item>
+      <DropdownMenu.Item>Edit Customer</DropdownMenu.Item>
+    </DropdownMenu.Content>
+  </DropdownMenu.Root>
