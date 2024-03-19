a9a92fb71ede17b9166bf412066da813d2d34487
royid_poulose <royidcp96@gmail.com>
2024-03-19 17:31:58 +0530

    create attribute design chage to shadcn

--- src/routes/(app)/attributes/createattributes/+page.svelte
+++ src/routes/(app)/attributes/createattributes/+page.svelte
@@ -5,6 +5,9 @@
     const dispatch = createEventDispatcher();
     import API from "$lib/services/api";
     import {Button }from "$lib/components/ui/button"
+    import {Label} from "$lib/components/ui/label"
+    import {Input} from "$lib/components/ui/input"
+    import * as Card from "$lib/components/ui/card"
 
     export let editData;
     export let editForm;
@@ -50,79 +53,66 @@
 </script>
 
 <div class="fixed bg-background inset-0 flex items-center justify-center" style="background-color: rgba(0, 0, 0, 0.5);">
-    <div class="bg-white p-6 rounded-lg">
+    <div class="flex items-center justify-center">
+      <div class="glow-border" >
+      <div class="card glow-border-content bg-background text-foreground">
         <!--        <div class="flex items-center justify-between mb-2">-->
-        <header class="font-bold mb-5" style="color: black">
-            {#if editForm === false}
-                <h1>New Attribute</h1>
-            {:else}
-                <h1>Update Attribute</h1>
-            {/if}
-            <!--            <button on:click={() => dispatch("close")}>-->
-            <!--                <i class="fa-solid fa-xmark"></i>-->
-            <!--            </button>-->
-        </header>
-        <!--        </div>-->
-        <!--        <div class="w-1/2">-->
-        <main>
-
-            <div>
-                <label
-                        for="name"
-                        class="block text-sm font-medium leading-6 text-gray-500 mb-2">Name</label
-                >
-                <div class="mb-2">
-                    <input required
-                           type="text"
-                           name="name"
-                           id="name"
-                           class="mb-2 block w-full rounded-md border-0 bg-white/5 py-1.5 px-4 outline-none text-gray-500 shadow-sm ring-1 ring-inset ring-gray-200 focus:ring-2 focus:ring-inset focus:ring-blue-500 sm:text-sm sm:leading-6"
-                           bind:value={name}
-                    />
+        <Card.Root>
+            <Card.Header class="font-bold mb-5" style="color: black">
+                {#if editForm === false}
+                    <Card.Title>New Attribute</Card.Title>
+                {:else}
+                    <Card.Title>Update Attribute</Card.Title>
+                {/if}
+                <!--            <button on:click={() => dispatch("close")}>-->
+                <!--                <i class="fa-solid fa-xmark"></i>-->
+                <!--            </button>-->
+            </Card.Header>
+            <Card.Content>
+                <div>
+                    <Label
+                            for="name"
+                            class="block text-sm font-medium leading-6 text-gray-500 mb-2">Name</Label>
+                    <div class="mb-2">
+                        <Input required
+                               type="text"
+                               name="name"
+                               id="name"
+                               bind:value={name}/>
+                    </div>
                 </div>
-            </div>
-            <div>
-                <label
-                        for="value"
-                        class="block text-sm font-medium leading-6 text-gray-500 mb-2">Value</label
-                >
-                <div class="mt-2">
-                    <input required
-                           type="text"
-                           name="value"
-                           id="value"
-                           class="block mb-2
-                     w-full rounded-md border-0 bg-white/5 py-1.5 px-4 outline-none text-gray-500 shadow-sm ring-1 ring-inset ring-gray-200 focus:ring-2 focus:ring-inset focus:ring-blue-500 sm:text-sm sm:leading-6"
-                           bind:value={value}
-                    />
-                    <span style="color: #17a2b8;">Kindly Use comma (',') to separate the values</span>
+                <div>
+                    <Label
+                            for="value"
+                            class="block text-sm font-medium leading-6 text-gray-500 mb-2">Value</Label>
+                    <div class="mt-2">
+                        <Input required
+                               type="text"
+                               name="value"
+                               id="value"
+                               bind:value={value}/>
+                        <span style="color: #17a2b8;">Kindly Use comma (',') to separate the values</span>
+                    </div>
                 </div>
-            </div>
-        </main>
-
-        <footer class="flex mt-5">
-            {#if editForm === false}
-                <Button type=submit  class="text-xs flex items-center gap-2 border border-blue-500 bg-blue-500 text-white px-4 py-1.5 rounded"
-                        on:click={() =>onCreateNewAttribute()}
-                >Create Attribute
-                </Button>
-
-            {:else}
-
-                <Button class="text-xs flex items-center gap-2 border border-blue-500 bg-blue-500 text-white px-4 py-1.5 rounded"
-                        on:click={() =>onCreateNewAttribute()}
-                >Update Attribute
-                </Button>
-            {/if}
-
-            <Button class="text-xs
-                                ml-2
-                               flex items-center gap-2
-                               border border-red-500 bg-red-500 text-white px-4 py-1.5 rounded"
-                    on:click={() => dispatch("close")}
-            >Cancel
-            </Button>
-        </footer>
+            </Card.Content>
+            <Card.Footer class="justify-between space-x-2">
+                    <Button variant="ghost"
+                            on:click={() => dispatch("close")}>
+                            Cancel
+                    </Button>
+                    {#if editForm === false}
+                        <Button type=submit
+                                on:click={() =>onCreateNewAttribute()}>
+                                Save
+                        </Button>
+                    {:else}
+                        <Button on:click={() =>onCreateNewAttribute()}>
+                                Update
+                        </Button>
+                    {/if}
+            </Card.Footer>
+        </Card.Root>
     </div>
-
+</div>
+</div>
 </div>
