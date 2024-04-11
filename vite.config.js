39a8bbab9d73082f462fadcb8cee286109fafbd6
Renoy C <48327628+capsmagick@users.noreply.github.com>
2024-04-11 22:07:36 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
 		},
 		proxy:{
 			'/api':{
-				target:"http://localhost:8000",
+				target:"http://manage.signupcasuals.com:8080",
 				changeOrigin: true,
 				rewrite: path => path.replace(/^\/api/, ''),
 			}
