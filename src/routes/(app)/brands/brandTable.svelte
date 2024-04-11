72c4f2be4e703b798c7347b4f4cd96c4b49874f1
jishnu <jishnuks9421@gmail.com>
2024-04-11 12:15:18 +0530

    hidable colums added to brand table

--- src/routes/(app)/brands/brandTable.svelte
+++ src/routes/(app)/brands/brandTable.svelte
@@ -58,6 +58,15 @@
   let editData: any;  
   let editForm: boolean = false;
 
+  let hidableCoulumns: any[] = [
+    {name: "Logo", value: true},
+    {name: "Description", value: true},
+    {name: "Created At", value: false},
+    {name: "Updated At", value: false},
+    {name: "Created By", value: false},
+    {name: "Updated By", value: false},
+  ];
+
 
     async function getBrands() {
     try {
@@ -168,17 +177,36 @@
 
 <div class="p-5">
   <div class="my-2 flex justify-between">
-    <Input
-        class="max-w-sm"
-        placeholder="Filter Brands..."
-        type="text"
-        on:input={(event) => searchName(event)}
-        />
+    <div>
+      <Input
+          class="max-w-sm"
+          placeholder="Search Brands..."
+          type="text"
+          on:input={(event) => searchName(event)}
+          />
+    </div>
+    <div class ="flex">
+      <div class="mr-2">
+        <DropdownMenu.Root>
+          <DropdownMenu.Trigger>
+            <Button variant="outline" size="sm">
+              Column
+              <ChevronDown class="ml-2 h-4 w-4"/>
+            </Button>
+          </DropdownMenu.Trigger>
+          <DropdownMenu.Content>
+            {#each hidableCoulumns as column}
+            <DropdownMenu.CheckboxItem bind:checked={column.value}>{column.name}</DropdownMenu.CheckboxItem>
+            {/each}
+          </DropdownMenu.Content>
+        </DropdownMenu.Root>
+      </div>
+      <div>
         <DropdownMenu.Root>
           <DropdownMenu.Trigger>
             <Button variant="outline" size="sm">
               {per_page}
-              <ChevronDown />
+              <ChevronDown class="ml-2 h-4 w-4"/>
             </Button>
           </DropdownMenu.Trigger>
           <DropdownMenu.Content>
@@ -189,6 +217,8 @@
             <DropdownMenu.Item on:click={(event) => pageLimit(event, 100)}>100</DropdownMenu.Item>
           </DropdownMenu.Content>
         </DropdownMenu.Root>
+      </div>
+    </div>
   </div>
   <Table.Root>
     <Table.Header>
@@ -196,16 +226,48 @@
         <Table.Head class="flex justify-between">Name
           <Button on:click={() => sortName()} variant="ghost"><CaretSort  class="w-4 h-4" /></Button>
         </Table.Head>
+        {#if hidableCoulumns[0].value}
         <Table.Head>Logo</Table.Head>
+        {/if}
+        {#if hidableCoulumns[1].value}
         <Table.Head>Description</Table.Head>
+        {/if}
+        {#if hidableCoulumns[2].value}
+        <Table.Head>Created At</Table.Head>
+        {/if}
+        {#if hidableCoulumns[3].value}
+        <Table.Head>Updated At</Table.Head>
+        {/if}
+        {#if hidableCoulumns[4].value}
+        <Table.Head>Created By</Table.Head>
+        {/if}
+        {#if hidableCoulumns[5].value}
+        <Table.Head>Updated By</Table.Head>
+        {/if}
         <Table.Head>Action</Table.Head>
       </Table.Row>
     </Table.Header>
       {#each tableData as data}
         <Table.Row>
           <Table.Cell>{data.name}</Table.Cell>
+          {#if hidableCoulumns[0].value}
           <Table.Cell>  <img src={data.logo} alt="Logo" class="w-12 h-12 object-cover rounded-full"></Table.Cell>
+          {/if}
+          {#if hidableCoulumns[1].value}
           <Table.Cell>{data.description}</Table.Cell>  
+          {/if}
+          {#if hidableCoulumns[2].value}
+          <Table.Cell>{data.created_at}</Table.Cell>  
+          {/if}
+          {#if hidableCoulumns[3].value}
+          <Table.Cell>{data.updated_at}</Table.Cell>  
+          {/if}
+          {#if hidableCoulumns[4].value}
+          <Table.Cell>{data.created_by}</Table.Cell>  
+          {/if}
+          {#if hidableCoulumns[5].value}
+          <Table.Cell>{data.updated_by}</Table.Cell>  
+          {/if}
           <Table.Cell>
              <DropdownMenu.Root>
                <DropdownMenu.Trigger asChild let:builder>
