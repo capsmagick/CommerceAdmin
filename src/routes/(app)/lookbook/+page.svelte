282f64bdad58b0539ddb368d4837a8efa98eb818
royid_poulose <royidcp96@gmail.com>
2024-04-25 03:49:03 +0530

    look book page

--- src/routes/(app)/lookbook/+page.svelte
+++ src/routes/(app)/lookbook/+page.svelte
@@ -3,18 +3,30 @@
   import LookbookTable from "./lookbookTable.svelte";
 
   let showForm: boolean = false;
+  function toggleForm() {
+    showForm = !showForm;
+  }
 </script>
 
 <div class="m-3 bg-background text-foreground rounded-md p-4 px-6 border">
   <div class="flex items-center">
     <h4
-      class="text-3xl font-bold tracking-tight text-gray-800 dark:text-gray-200 flex-1"
-    >
+      class="text-3xl font-bold tracking-tight text-gray-800 dark:text-gray-200 flex-1">
       Lookbook
     </h4>
     <div class="glow-border mr-4">
-      <Button variant="outline" class="glow-border-content">Export</Button>
+      <Button variant="outline" class="glow-border-content">Export Lookbook</Button>
+    </div>
+    <div class="glow-border mr-4">
+      <Button
+        variant="outline"
+        class="text-xs flex items-center gap-2  px-4 py-1.5"
+        on:click={() => toggleForm()}
+      >
+        <i class="fa-solid fa-cloud-arrow-up text-sm"></i>
+        Add Lookbook</Button
+      >
     </div>
   </div>
-  <LookbookTable {showForm} />
+  <LookbookTable {showForm} on:cancel={() => (showForm = false)}/>
 </div>
