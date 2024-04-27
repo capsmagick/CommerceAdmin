7f97923c3d8be5389b7bd0ecf7dfe40c0947e83d
jishnu <jishnuks9421@gmail.com>
2024-04-27 17:01:19 +0530

    enabled login while click enter in the password input field

--- src/routes/(auth)/login/+page.svelte
+++ src/routes/(auth)/login/+page.svelte
@@ -158,6 +158,11 @@
             autocomplete="current-password"
             required
             class="block pl-4 w-full rounded-md  border-0 py-1.5  shadow-sm ring-1 ring-inset ring-gray-300  focus:ring-2 focus:ring-inset focus:ring-violet-600 sm:text-sm sm:leading-6"
+            on:keydown={(event) => {
+            if (event.key === "Enter") {
+              loginFn(); 
+            }
+          }}
           />
           {#if passwordError}
           <p class="text-red-500 mt-1 text-sm">{passwordError}</p>
